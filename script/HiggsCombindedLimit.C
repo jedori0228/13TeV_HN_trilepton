dc6edd074906837ce395365e41a508d19b47f985
Jae Sung Kim <jae.sung.kim@cern.ch>
2017-04-05 11:42:42 +0900

    Higgs combined tool

---
+++ script/HiggsCombindedLimit.C
@@ -0,0 +1,139 @@
+#include "canvas_margin.h"
+
+void HiggsCombindedLimit(){
+
+  gStyle->SetOptTitle(0);
+
+  TString catversion = getenv("CATVERSION");
+  TString dataset = getenv("CATANVERSION");
+  TString WORKING_DIR = getenv("PLOTTER_WORKING_DIR");
+
+  TString filepath = WORKING_DIR+"/plots/"+dataset+"/LimitOutput/HiggsCombinedTools/";
+  TString plotpath = WORKING_DIR+"/plots/"+dataset+"/LimitOutput/HiggsCombinedTools/";
+
+  TString dirname = "170405";
+  filepath += dirname+"/";
+  plotpath += dirname+"/";
+
+  TLatex latex_CMSPriliminary, latex_Lumi;
+  latex_CMSPriliminary.SetNDC();
+  latex_Lumi.SetNDC();
+
+  //==== read result file
+  string elline;
+  ifstream in(filepath+"/result.txt");
+
+  double mass[17], limit[17], onesig_left[17], onesig_right[17], twosig_left[17], twosig_right[17];
+
+  int dummyint=0;
+  while(getline(in,elline)){
+    std::istringstream is( elline );
+
+    is >> mass[dummyint];
+    is >> limit[dummyint];
+    is >> onesig_left[dummyint];
+    is >> onesig_right[dummyint];
+    is >> twosig_left[dummyint];
+    is >> twosig_right[dummyint];
+
+    double scale=1.;
+    if(mass[dummyint]<=80) scale = 0.00001;
+    else if(mass[dummyint]<=100) scale = 0.0001;
+    else if(mass[dummyint]<=700) scale = 0.001;
+    else scale = 0.001;
+
+    limit[dummyint] *= scale;
+    onesig_left[dummyint] *= scale;
+    onesig_right[dummyint] *= scale;
+    twosig_left[dummyint] *= scale;
+    twosig_right[dummyint] *= scale;
+
+    onesig_left[dummyint] = limit[dummyint]-onesig_left[dummyint];
+    onesig_right[dummyint] = onesig_right[dummyint] - limit[dummyint];
+    twosig_left[dummyint] = limit[dummyint]-twosig_left[dummyint];
+    twosig_right[dummyint] = twosig_right[dummyint] - limit[dummyint];
+
+    dummyint++;
+  }
+
+  TGraph *gr_13TeV_exp = new TGraph(17,mass,limit);
+  gr_13TeV_exp->SetLineWidth(3);
+  gr_13TeV_exp->SetLineStyle(2);
+  gr_13TeV_exp->SetFillColor(kWhite);
+
+  TGraphAsymmErrors *gr_band_1sigma = new TGraphAsymmErrors(17, mass, limit, 0, 0, onesig_left, onesig_right);
+  gr_band_1sigma->SetFillColor(kGreen);
+  gr_band_1sigma->SetLineColor(kGreen);
+  gr_band_1sigma->SetMarkerColor(kGreen);
+
+  TGraphAsymmErrors *gr_band_2sigma = new TGraphAsymmErrors(17, mass, limit, 0, 0, twosig_left, twosig_right);
+  gr_band_2sigma->SetFillColor(kYellow);
+  gr_band_2sigma->SetLineColor(kYellow);
+  gr_band_2sigma->SetMarkerColor(kYellow);
+
+  TLegend *lg = new TLegend(0.50, 0.15, 0.90, 0.50);
+
+
+
+  lg->AddEntry(gr_13TeV_exp,"CL_{s} Expected");
+  lg->AddEntry(gr_band_1sigma,"CL_{s} Expected #pm 1 #sigma");
+  lg->AddEntry(gr_band_2sigma,"CL_{s} Expected #pm 2 #sigma");
+
+  const int nm = 16;
+  double mass_8TeV[nm] = {
+    40, 50, 60, 70, 80,
+    90, 100, 125, 150, 175, 
+    200, 250, 300, 350, 400, 500
+  };
+  double exp[nm];
+
+  vector<double> MixingValues8TeV = {
+    2.82635E-05, 3.69206E-05, 8.52678E-05, 0.000618594, 0.00148793,
+    0.00583953, 0.00257802, 0.00290533, 0.00449771, 0.00722055,
+    0.0108741, 0.0194395, 0.0400305, 0.0708397, 0.149737, 0.497138
+  };
+  for(unsigned int j=0; j<MixingValues8TeV.size(); j++) exp[j] = MixingValues8TeV.at(j);
+
+  TGraph *gr_8TeV_exp = new TGraph(nm, mass_8TeV, exp);
+  gr_8TeV_exp->SetLineColor(kRed);
+  gr_8TeV_exp->SetLineWidth(3);
+
+  TCanvas *c_out = new TCanvas("c_out", "", 800, 800);
+  canvas_margin(c_out);
+  c_out->cd();
+  c_out->Draw();
+  c_out->SetLogy();
+  c_out->SetGridx();
+  c_out->SetGridy();
+
+  gr_band_2sigma->Draw("A3");
+  hist_axis(gr_band_2sigma);
+  gr_band_2sigma->GetYaxis()->SetTitle("|V_{#muN}^{2}|");
+  gr_band_2sigma->GetXaxis()->SetTitle("m(HN) [GeV]");
+  gr_band_2sigma->GetYaxis()->SetTitleSize(0.06); 
+  gr_band_2sigma->GetYaxis()->SetRangeUser(0.00002, 10.);
+  gr_band_2sigma->SetTitle("");
+  gr_band_1sigma->Draw("3same");
+  gr_13TeV_exp->Draw("lsame");
+  gr_8TeV_exp->Draw("lsame");
+
+  lg->AddEntry(gr_8TeV_exp, "8 TeV DiMuon Ch. Expected", "l");
+  lg->SetX2NDC(0.90);
+  lg->SetY2NDC(0.67);
+  lg->SetBorderSize(0);
+  lg->SetFillStyle(0);
+  lg->Draw();
+
+  latex_CMSPriliminary.SetTextSize(0.035);
+  latex_CMSPriliminary.DrawLatex(0.15, 0.96, "#font[62]{CMS} Simulation");
+  latex_Lumi.SetTextSize(0.035);
+  latex_Lumi.DrawLatex(0.7, 0.96, "35.9 fb^{-1} (13 TeV)");
+
+  c_out->SaveAs(plotpath+"/13TeV_mixing.pdf");
+
+  c_out->Close();
+
+
+
+
+}
---
+++ script/write_limit_datacard.py
@@ -0,0 +1,121 @@
+import os,math
+import ROOT
+
+Lumi = 35863.308
+
+WORKING_DIR = os.environ["PLOTTER_WORKING_DIR"]
+dataset = os.environ["CATANVERSION"]
+LIMIT_PATH = WORKING_DIR+"/plots/"+dataset+"/RootfileForLimit/"
+DATACARDS_PATH = LIMIT_PATH+"datacards"
+
+f = ROOT.TFile(LIMIT_PATH+"/hist.root")
+
+hist_mass = f.Get("hist_mass")
+hist_xsec = f.Get("hist_xsec")
+hist_fake = f.Get("hist_fake")
+hist_prompt = f.Get("hist_prompt")
+hist_signal = f.Get("hist_signal")
+hist_signal_weighted = f.Get("hist_signal_weighted")
+
+kfactor_haneol = [
+    1.08, 1.08, 1.08, 1.09,
+    1.09, 1.09, 1.09, 1.09,
+    1.11, 1.12, 1.14, 1.15,
+    1.17, 1.18, 1.18, 1.19,
+    1.19]
+
+
+for i in range(0,17):
+
+  mass = str(int(hist_mass.GetBinContent(i+1)))
+
+  print ""
+  print "#### HN"+mass+" ####"
+  print ""
+
+  xsec = hist_xsec.GetBinContent(i+1)
+
+  fake = hist_fake.GetBinContent(i+1)
+  fake_err = hist_fake.GetBinError(i+1)
+
+  prompt = hist_prompt.GetBinContent(i+1)
+  prompt_err = hist_prompt.GetBinError(i+1)
+
+  signal = hist_signal.GetBinContent(i+1)
+  signal_err = hist_signal.GetBinError(i+1)
+
+  signal_weighted = hist_signal_weighted.GetBinContent(i+1)*200.
+  signal_weighted_err = hist_signal_weighted.GetBinError(i+1)*200.
+
+  if hist_mass.GetBinContent(i+1) <= 80:
+    signal_weighted *= 0.00001
+    signal_weighted_err *= 0.00001
+  elif hist_mass.GetBinContent(i+1) <= 100:
+    signal_weighted *= 0.0001
+    signal_weighted_err *= 0.0001
+  elif hist_mass.GetBinContent(i+1) <= 700:
+    signal_weighted *= 0.001
+    signal_weighted_err *= 0.001
+  else:
+    signal_weighted *= 0.001
+    signal_weighted_err *= 0.001
+
+  signal_weighted *= kfactor_haneol[i]
+  signal_weighted_err *= kfactor_haneol[i]
+
+  N_MC = 100000.;
+  if mass=="200":
+    N_MC = 96193.;
+  if mass=="400":
+    N_MC = 99070.;
+
+  signal_yield = 0.01*signal*N_MC*200.
+  signal_yield_err = 1.+(0.01*signal_err*N_MC*200.)/signal_yield
+
+  bkg_yield = fake+prompt
+  bkg_yield_err = 1.+math.sqrt(fake_err*fake_err+prompt_err*prompt_err)/bkg_yield
+
+  out = open(DATACARDS_PATH+"/HN"+mass+".txt","w")
+
+  #print "bin\t1\t1"
+  #print "process\tHN"+mass+"\tbkgd"
+  #print "process\t0\t1"
+  #print "rate\t"+str(signal_weighted)+"\t"+str(bkg_yield)
+  #print "------------"
+  #print "lumi\tlnN\t1.026\t1.026"
+  #print "sigerr\tlnN\t"+str(signal_yield_err)+"\t-"
+  #print "bkgerr\tlnN\t-\t"+str(bkg_yield_err)
+
+  out.write("imax 1  number of channels\n") 
+  out.write("jmax 1  number of backgrounds\n") 
+  out.write("kmax 3  number of nuisance parameters (sources of systematical uncertainties)\n") 
+  out.write("------------\n") 
+  out.write("# we have just one channel, in which we observe 0 events\n") 
+  out.write("bin 1\n") 
+  out.write("observation 0\n") 
+  out.write("------------\n") 
+  out.write("# now we list the expected events for signal and all backgrounds in that bin\n") 
+  out.write("# the second 'process' line must have a positive number for backgrounds, and 0 for signal\n") 
+  out.write("# then we list the independent sources of uncertainties, and give their effect (syst. error)\n") 
+  out.write("# on each process and bin\n") 
+  out.write("bin\t1\t1\n")
+  out.write("process\tHN"+mass+"\tbkgd\n")
+  out.write("process\t0\t1\n")
+  out.write("rate\t"+str(signal_weighted)+"\t"+str(bkg_yield)+"\n")
+  out.write("------------\n")
+  out.write("lumi\tlnN\t1.026\t1.026\n")
+  out.write("sigerr\tlnN\t"+str(signal_yield_err)+"\t-\n")
+  out.write("bkgerr\tlnN\t-\t"+str(bkg_yield_err)+"\n")
+  out.close()
+
+
+
+
+#  KEY: TH1D  hist_mass;1  
+#  KEY: TH1D  hist_xsec;1  
+#  KEY: TH1D  hist_fake;1  
+#  KEY: TH1D  hist_prompt;1  
+#  KEY: TH1D  hist_signal;1
+
+  
+
