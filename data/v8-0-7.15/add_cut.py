d1819c9a45d08d0207ea27b67667585fed011438
Jae Sung Kim <jae.sung.kim@cern.ch>
2017-06-22 09:32:30 +0900

    data for v8-0-7.15..

---
+++ data/v8-0-7.15/add_cut.py
@@ -0,0 +1,28 @@
+import os
+
+#filenames=['SR_rebins.txt', 'SR_xaxis.txt', 'SR_yaxis.txt'] 
+filenames=['CR_rebins.txt', 'CR_xaxis.txt', 'CR_yaxis.txt'] 
+
+From = "_ZGamma_3mu0el"
+To = "_ZGamma_2mu1el"
+
+for filename in filenames:
+
+  tempfile = open('test_'+filename,'w')
+
+  lines = open(filename).readlines()
+
+  savethem=[]
+
+  for line in lines:
+    if From in line:
+      savethem.append(line)
+    tempfile.write(line)
+
+  for line in savethem:
+    line = line.replace(From,To)
+    tempfile.write(line)
+
+  tempfile.close()
+  os.system('mv test_'+filename+' '+filename)
+  
